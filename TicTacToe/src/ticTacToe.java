53d222f4d6324bbb57ed05a9810c4039b130fa6c
kayn0111034 <0111034@student.isf.edu.hk>
2021-07-25 22:59:00 +0800

    'x' appears when button is clicked

--- TicTacToe/src/ticTacToe.java
+++ TicTacToe/src/ticTacToe.java
@@ -15,35 +15,34 @@ import javax.swing.JPanel;
 public class ticTacToe implements ActionListener{
 	
 	private int count=0;
-	private JLabel label;
+	//private JLabel label;
 	private JFrame frame;
 	private JPanel panel;
+	JButton button1= new JButton();
+	JButton button2= new JButton();
+	JButton button3= new JButton();
+	JButton button4= new JButton();
+	JButton button5= new JButton();
+	JButton button6= new JButton();
+	JButton button7= new JButton();
+	JButton button8= new JButton();
+	JButton button9= new JButton();
 	
 	public ticTacToe() {
 		frame= new JFrame();
 		panel= new JPanel();
+		//label= new JLabel("Num of Clicks:0");
 		//frame.pack();
 		frame.setPreferredSize(new Dimension(400,400));
-		JButton button1= new JButton("Hello World");
 		button1.addActionListener(this);
-		JButton button2= new JButton("Hello World");
 		button2.addActionListener(this);
-		JButton button3= new JButton("Hello World");
 		button3.addActionListener(this);
-		JButton button4= new JButton("Hello World");
 		button4.addActionListener(this);
-		JButton button5= new JButton("Hello World");
 		button5.addActionListener(this);
-		JButton button6= new JButton("Hello World");
 		button6.addActionListener(this);
-		JButton button7= new JButton("Hello World");
 		button7.addActionListener(this);
-		JButton button8= new JButton("Hello World");
 		button8.addActionListener(this);
-		JButton button9= new JButton("Hello World");
 		button9.addActionListener(this);
-		
-		
 		
 		panel.setBorder(BorderFactory.createLineBorder(Color.black));
 		panel.setLayout(new GridLayout(3,3));
@@ -61,7 +60,7 @@ public class ticTacToe implements ActionListener{
 		
 		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Hello World");
+		frame.setTitle("Tic Tac Toe");
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -77,6 +76,25 @@ public class ticTacToe implements ActionListener{
 		// TODO Auto-generated method stub
 		//count+=1;
 		//label.setText("Number of clicks: "+count);
+		//JButton button10 = new JButton("wiee");
+		if(e.getSource()==button1)
+			button1.setText("X");
+		else if(e.getSource()==button2)
+			button2.setText("X");
+		else if(e.getSource()==button3)
+			button3.setText("X");
+		else if(e.getSource()==button4)
+			button4.setText("X");
+		else if(e.getSource()==button5)
+			button5.setText("X");
+		else if(e.getSource()==button6)
+			button6.setText("X");
+		else if(e.getSource()==button7)
+			button7.setText("X");
+		else if(e.getSource()==button8)
+			button8.setText("X");
+		else if(e.getSource()==button9)
+			button9.setText("X");
 	}
 
 }
